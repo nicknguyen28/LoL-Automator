1c675059da3956c3c174f2b05a0d8bc5b6871fb1
Nick Nguyen <s2081459@tamu.edu>
2022-10-12 12:18:35 -0500

    Make cursor move to accept button using accept.png

--- automator.py
+++ automator.py
@@ -1,7 +1,13 @@
 import pyautogui
 from time import sleep
+# pyautogui.FAILSAFE = True
 
 while True:
-    print("Loop")
-    print(pyautogui.position())
-    sleep(2)
+    print("Looping!")
+    if (pyautogui.position().x == 0 and pyautogui.position().y == 0):
+        print("Ending program!")
+        break
+    acceptXY = pyautogui.locateCenterOnScreen("accept.png", confidence = 0.5)
+    if acceptXY != None:
+        pyautogui.moveTo(acceptXY, duration = 0.5)
+    sleep(3)
