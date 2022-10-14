395d9148ac609f29d148b955e2574f92915c61b1
Nick Nguyen <s2081459@tamu.edu>
2022-10-14 14:53:24 -0500

    Add functionality for select and ban, with placeholders for png files

--- automator.py
+++ automator.py
@@ -1,54 +1,88 @@
 import sys
 import pyautogui
 import tkinter as tk
+from time import sleep
+
+global stringSelect
+global stringBan
+stringSelect = ""
+stringBan = ""
 
 def accept():
     if buttonAccept.config('text')[-1] == "Enabled":
         print("Searching!")
         acceptXY = pyautogui.locateCenterOnScreen("accept.png", confidence = 0.5)
         if acceptXY != None:
-            buttonAccept.config(text="Disabled")
+            buttonAccept.config(text = "Disabled")
             pyautogui.moveTo(acceptXY, duration = 0.5)
             pyautogui.click()
         root.after(3000, accept)
 
 def select():
     if buttonSelect.config('text')[-1] == "Enabled":
-        print("Searching!")
-        acceptXY = pyautogui.locateCenterOnScreen("accept.png", confidence = 0.5)
-        if acceptXY != None:
-            buttonSelect.config(text="Disabled")
-            pyautogui.moveTo(acceptXY, duration = 0.5)
-        root.after(3000, accept)
+        print("Selecting!")
+        stringSelect = entrySelect.get()
+        searchBoxXY = pyautogui.locateCenterOnScreen("searchBox.png", confidence = 0.5)
+        greyLockInXY = pyautogui.locateCenterOnScreen("greyedOutLockIn.png", confidence = 0.5)
+        if greyLockInXY != None and searchBoxXY != None:
+            buttonSelect.config(text = "Disabled")
+            pyautogui.moveTo(searchBoxXY, duration = 0.5)
+            pyautogui.click()
+            sleep(1)
+            pyautogui.typewrite(stringSelect, interval = 0.25)
+            pyautogui.moveTo(searchBoxXY.x - 100, searchBoxXY.y - 100, duration = 0.5)
+            pyautogui.click()
+            sleep(1)
+            lockInXY = pyautogui.locateCenterOnScreen("lockIn.png", confidence = 0.5)
+            if (lockInXY != None):
+                pyautogui.moveTo(lockInXY)
+                pyautogui.click()
+            else:
+                pyautogui.alert("Champion was not available. Please enter a new one.")
+        root.after(3000, select)
 
 def ban():
     if buttonBan.config('text')[-1] == "Enabled":
-        print("Searching!")
-        acceptXY = pyautogui.locateCenterOnScreen("accept.png", confidence = 0.5)
-        if acceptXY != None:
-            buttonBan.config(text="Disabled")
-            pyautogui.moveTo(acceptXY, duration = 0.5)
-        root.after(3000, accept)
+        print("Banning!")
+        stringBan = entryBan.get()
+        searchBoxXY = pyautogui.locateCenterOnScreen("searchBox.png", confidence = 0.5)
+        greyBanXY = pyautogui.locateCenterOnScreen("greyedOutBan.png", confidence = 0.5)
+        if greyBanXY != None and searchBoxXY != None:
+            buttonBan.config(text = "Disabled")
+            pyautogui.moveTo(searchBoxXY, duration = 0.5)
+            pyautogui.click()
+            sleep(1)
+            pyautogui.typewrite(stringBan, interval = 0.25)
+            pyautogui.moveTo(searchBoxXY.x - 100, searchBoxXY.y - 100, duration = 0.5)
+            pyautogui.click()
+            sleep(1)
+            banXY = pyautogui.locateCenterOnScreen("ban.png", confidence = 0.5)
+            if (banXY != None):
+                pyautogui.moveTo(banXY)
+                pyautogui.click()
+            else:
+                pyautogui.alert("Champion was not available. Please enter a new one.")
+        root.after(3000, select)
 
 def toggleAcceptButton():
-    if buttonAccept.config('text')[-1] == "Enabled":
-        buttonAccept.config(text="Disabled")
+    if buttonAccept.config("text")[-1] == "Enabled":
+        buttonAccept.config(text = "Disabled")
     else:
-        buttonAccept.config(text="Enabled")
+        buttonAccept.config(text = "Enabled")
         root.after(3000, accept)
 
 def toggleSelectButton():
-    if buttonSelect.config('text')[-1] == "Enabled":
-        buttonSelect.config(text="Disabled")
+    if buttonSelect.config("text")[-1] == "Enabled":
+        buttonSelect.config(text = "Disabled")
     else:
-        buttonSelect.config(text="Enabled")
+        buttonSelect.config(text = "Enabled")
         root.after(3000, select)
 
 def toggleBanButton():
-    if buttonBan.config('text')[-1] == "Enabled":
-        buttonBan.config(text="Disabled")
+    if buttonBan.config("text")[-1] == "Enabled":
+        buttonBan.config(text = "Disabled")
     else:
-        buttonBan.config(text="Enabled")
+        buttonBan.config(text = "Enabled")
         root.after(3000, ban)
 
 def getText():
@@ -60,16 +94,18 @@ def getText():
 root= tk.Tk()
 root.title("Auto Acceptor")
 canvas = tk.Canvas(root, width = 400, height = 400)
-labelAccept = tk.Label(root, text="Auto accept match")
+labelAccept = tk.Label(root, text = "Auto accept match")
 buttonAccept = tk.Button(pady = 5, padx = 30, text = "Disabled", bg = "black", fg = "white", command = toggleAcceptButton)
 
-labelSelect = tk.Label(root, text="Auto select champion")
-entrySelect = tk.Entry(root, width= 40)
+labelSelect = tk.Label(root, text = "Auto select champion")
+global entrySelect
+entrySelect = tk.Entry(root, width = 40)
 entrySelect.focus_set()
 buttonSelect = tk.Button(pady = 5, padx = 30, text = "Disabled", bg = "black", fg = "white", command = toggleSelectButton)
 
-labelBan = tk.Label(root, text="Auto ban champion")
-entryBan = tk.Entry(root, width= 40)
+labelBan = tk.Label(root, text = "Auto ban champion")
+global entryBan
+entryBan = tk.Entry(root, width = 40)
 entryBan.focus_set()
 buttonBan = tk.Button(pady = 5, padx = 30, text = "Disabled", bg = "black", fg = "white", command = toggleBanButton)
 
