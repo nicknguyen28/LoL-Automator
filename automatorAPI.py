6cd071adcc542ff964a6d755ab3bb67503158897
Nick Nguyen <s2081459@tamu.edu>
2022-10-24 13:02:00 -0500

    Initial commit of automator using API

---
+++ automatorAPI.py
@@ -0,0 +1,22 @@
+from lcu_driver import Connector
+
+connector = Connector()
+
+
+@connector.ready
+async def connect(connection):
+    print('LCU API is ready to be used.')
+    summoner = await connection.request('get', '/lol-summoner/v1/current-summoner')
+    print(await summoner.json())
+
+@connector.close
+async def disconnect(connection):
+    print('Finished task')
+    await connector.stop()    
+
+@connector.ws.register('/lol-matchmaking/v1/ready-check', event_types = ('UPDATE',))
+async def readyCheck(connection, event):
+    if event.data['state'] == 'InProgress' and event.data['playerResponse'] == 'None':
+        await connection.request('post', '/lol-matchmaking/v1/ready-check/accept')
+
+connector.start()
